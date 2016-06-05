cbb68c57ad3feaed69e7f15cc7eaf9df0fd36ecf
Jordan S Read <jread@usgs.gov>
2016-06-04 19:55:18 -0500

    data for #6

---
+++ scripts/2_munge/munge_bass_wally_decline.R
@@ -0,0 +1,2 @@
+wally <- read.csv('data/walleye_age0_annual_means.csv', stringsAsFactors = FALSE, header = TRUE)
+bass <- read.csv('data/fall_lmb8_cpue_annual_mean.csv', stringsAsFactors = FALSE, header = TRUE)
