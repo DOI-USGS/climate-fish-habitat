56d79b5df9602cfb6591b6ddd36acc3990c469f4
Jordan S Read <jread@usgs.gov>
2016-06-18 21:41:05 -0500

    line mousers

--- scripts/2_munge/sankey_fig.R
+++ scripts/2_munge/sankey_fig.R
@@ -19,7 +19,7 @@ function hovertext(text, evt){
 var tooltip = document.getElementById("tooltip");
 var tooltip_bg = document.getElementById("tooltip_bg");
 tooltip.setAttribute("text-anchor","begin");
-tooltip.setAttribute("dx","0.5em");
+tooltip.setAttribute("dx","7");
 if (evt === undefined){
 tooltip.setAttribute("class","hidden");
 tooltip.firstChild.data = text;
@@ -36,10 +36,10 @@ tooltip_bg.setAttribute("y",pt.y-20);
 tooltip.setAttribute("class","shown");
 tooltip_bg.setAttribute("class","shown");
 var length = tooltip.getComputedTextLength();
-tooltip_bg.setAttribute("width", length+6);
+tooltip_bg.setAttribute("width", length+8);
 if (pt.x+length+8 > xmax){
 tooltip.setAttribute("text-anchor","end");
-tooltip.setAttribute("dx","-0.5em");
+tooltip.setAttribute("dx","-4");
 tooltip_bg.setAttribute("x",pt.x-8-length);
 }
 }}
@@ -52,6 +52,7 @@ types = c('Walleye dominant', 'Coexistence', 'Bass dominant', 'Neither')
 colors = c('cyan','#9932CD','#cc0000','grey')
 to.types = c('toWally', 'toCoexistence', 'toBass', 'toNeither')
 periods = c('early'='X1989.2014', 'mid'='X2040.2064', 'late'='X2065.2089')
+arrow.names <- c('toWally'='Walleye dominant', 'toCoexistence'='Coexistence', 'toBass'='Bass dominant', 'toNeither'="Neither")
 period.txt = c('1989-2014','2040-2064','2065-2089')
 library(dinosvg)
 svg_node = dinosvg:::svg_node
@@ -188,9 +189,16 @@ for (i in 1:2){
   for (from.type in names(start.arrows[[period.from]])){
     for (to.type in names(start.arrows[[period.from]][[from.type]])){
       stc = start.arrows[[period.from]][[from.type]][[to.type]]
+      arr.txt <- arrow.names[[to.type]]
+      if (from.type == arr.txt){
+        mouse.text <- sprintf("hovertext('%s lakes remain as %s',evt)",stc[['h']]/scale, from.type)
+      } else {
+        mouse.text <- sprintf("hovertext('%s lakes shift from %s to %s',evt)",stc[['h']]/scale, from.type, arr.txt)  
+      }
+      
       svg_node('path', g, c(d = sprintf("M%s,%s L%s,%s v-%s L%s,%s", box.w, stc[['y1']], box.w+gap.s, stc[['y2']], stc[['h']], box.w, stc[['y1']]-stc[['h']]), 
                             fill=colors[from.i], stroke='none', opacity="0.2",
-                            onmousemove=sprintf("hovertext('%s %s lakes %s ',evt)",stc[['h']]/scale, from.type, to.type), onmouseout="hovertext(' ')"))
+                            onmousemove=mouse.text, onmouseout="hovertext(' ')"))
     }
     from.i = from.i+1
   }
@@ -198,7 +206,7 @@ for (i in 1:2){
 }
 
 svg_node('rect',svg, c(id="tooltip_bg", rx="2.5", ry="2.5", width="55", height="20", fill="white", 'stroke-width'="0.5", stroke="#696969", class="hidden"))
-svg_node('text',svg, c(id="tooltip", dx="0.5em", dy="-5", stroke="none", fill="#000000", 'text-anchor'="begin", class="sub-label"), XML::newXMLTextNode(' '))
+svg_node('text',svg, c(id="tooltip", dy="-5", stroke="none", fill="#000000", 'text-anchor'="begin", class="sub-label"), XML::newXMLTextNode(' '))
 
 
 dinosvg:::write_svg(svg, file='sandbox/futureSuitability-desktop.svg')
