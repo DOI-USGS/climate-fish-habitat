faabe243a42f1bd34172c0ac8abfa2198a8b11af
Jordan S Read <jread@usgs.gov>
2016-06-19 21:06:50 -0500

    blank boxes above text and tooltip

--- scripts/2_munge/sankey_fig.R
+++ scripts/2_munge/sankey_fig.R
@@ -6,7 +6,7 @@ arrows.1 = group_by(fish.change, X1989.2014) %>%
   summarize(toCoexistence=sum(X2040.2064 == 'Coexistence'),toWally=sum(X2040.2064 == 'Walleye dominant'), 
             toBass = sum(X2040.2064 == 'Bass dominant'), toNeither=sum(X2040.2064 == 'Neither'))
 
-
+min.h <- 3 #px
 js.funs <- '\nvar svg = document.querySelector("svg")
     var xmax = Number(svg.getAttribute("viewBox").split(" ")[2]);
 var pt = svg.createSVGPoint();
@@ -14,6 +14,9 @@ function init(evt){
 if ( window.svgDocument == null ) {
 svgDocument = evt.target.ownerDocument;
 }
+}
+function changeOpacity(id, val){
+  \tdocument.getElementById(id).setAttribute("opacity", val);
 }
 function hovertext(text, evt){
 var tooltip = document.getElementById("tooltip");
@@ -27,7 +30,6 @@ tooltip_bg.setAttribute("x",0);
 tooltip_bg.setAttribute("y",0);
 tooltip_bg.setAttribute("class","hidden");
 } else {
-evt.target.setAttribute("opacity","1.0");
 pt = cursorPoint(evt)
 tooltip.setAttribute("x",pt.x);
 tooltip.setAttribute("y",pt.y);
@@ -50,7 +52,7 @@ return pt.matrixTransform(svg.getScreenCTM().inverse());
 };'
 
 types = c('Walleye dominant', 'Coexistence', 'Bass dominant', 'Neither')
-colors = c('Walleye dominant'='cyan','Coexistence'='#9932CD','Bass dominant'='#cc0000','Neither'='grey')
+colors = c('Walleye dominant'='#01b29F','Coexistence'='#9932CD','Bass dominant'='#990000','Neither'='grey')
 to.types = c('toWally', 'toCoexistence', 'toBass', 'toNeither')
 periods = c('early'='X1989.2014', 'mid'='X2040.2064', 'late'='X2065.2089')
 arrow.names <- c('toWally'='Walleye dominant', 'toCoexistence'='Coexistence', 'toBass'='Bass dominant', 'toNeither'="Neither")
@@ -94,19 +96,23 @@ font-size: 20px;
                   ')
 
 dinosvg:::add_ecmascript(svg, js.funs)
-
+blank.period.g <- svg_node('g',NULL, c('id'='mouseover-periods','opacity'="0"))
 for (i in 1:length(periods)){
   period.id = names(periods)[i]
   period.name = unname(periods[i])
   
-  g <- svg_node('g',svg, c(id=period.id, opacity="0.9",transform=sprintf("translate(%s,%s)",l.m+(i-1)*(box.w+gap.s), t.m)))
+  g <- svg_node('g',svg, c(id=period.id, transform=sprintf("translate(%s,%s)",l.m+(i-1)*(box.w+gap.s), t.m)))
+  g.blank <- svg_node('g',blank.period.g, c(id=paste0(period.id,'-blank'), transform=sprintf("translate(%s,%s)",l.m+(i-1)*(box.w+gap.s), t.m)))
   y[[period.name]][1] = 0
   t = 1
   period.data = group_by_(fish.change, period.name) %>% tally %>% data.frame %>% tidyr::spread_(key=period.name, 'n')
   for (type in types){
+    id <- paste0(period.name, '-', strsplit(type,'[ ]')[[1]][1])
     h[[period.name]][t] = period.data[[type]]*scale
-    svg_node('rect',g,c(width=box.w, class='bin', height=h[[period.name]][t], y=y[[period.name]][t], fill=colors[[type]], opacity="0.6",
-                        onmousemove=sprintf("hovertext('%s %s lakes',evt)",formatC(period.data[[type]], format="d", big.mark=','), type), onmouseout="hovertext(' ');evt.target.setAttribute('opacity','0.6');"))
+    svg_node('rect',g,c(width=box.w, class='bin', height=h[[period.name]][t], y=y[[period.name]][t], fill=colors[[type]], opacity="0.8", id=id))
+    svg_node('rect',g.blank,c(width=box.w, height=h[[period.name]][t], y=y[[period.name]][t],
+                        onmousemove=sprintf("hovertext('%s %s lakes',evt);changeOpacity('%s','1.0');",formatC(period.data[[type]], format="d", big.mark=','), type,id), 
+                        onmouseout=sprintf("hovertext(' ');changeOpacity('%s','0.8');",id)))
     if (period.data[[type]] < n.threshold[1]){
       svg_node('text',g, c(x=box.w/2, y=y[[period.name]][t], dy="-3", fill='black', stroke='none', 'text-anchor'='middle'), XML::newXMLTextNode(sprintf("%s",type)))
     } else if (period.data[[type]] > n.threshold[2]){
@@ -184,9 +190,13 @@ for (i in 2:length(periods)){
   }
 }
 
+
+blank.arrow.g <- svg_node('g',NULL, c('id'='mouseover-arrows','opacity'="0"))
+
 for (i in 1:2){
   period.from = unname(periods[i])
   g <- svg_node('g',svg, c(opacity='0.4', id=paste0(period.from,'-arrow'), transform=sprintf("translate(%s,%s)",l.m+(i-1)*(box.w+gap.s), t.m)))
+  g.blank <- svg_node('g', blank.arrow.g, c(id=paste0(period.from,'-arrow-blank'), transform=sprintf("translate(%s,%s)",l.m+(i-1)*(box.w+gap.s), t.m)))
   for (from.type in c('Bass dominant', 'Neither', 'Walleye dominant', 'Coexistence')){
     for (to.type in names(start.arrows[[period.from]][[from.type]])){
       stc = start.arrows[[period.from]][[from.type]][[to.type]]
@@ -196,10 +206,14 @@ for (i in 1:2){
       } else {
         mouse.text <- sprintf("hovertext('%s lakes shift from %s to %s',evt)",formatC(stc[['h']]/scale, format="d", big.mark=','), from.type, arr.txt)  
       }
+      id <- paste0(period.from,'-',strsplit(from.type,'[ ]')[[1]][1],'-',strsplit(arr.txt,'[ ]')[[1]][1])
       
       svg_node('path', g, c(d = sprintf("M%s,%s L%s,%s v-%s L%s,%s", box.w, stc[['y1']], box.w+gap.s, stc[['y2']], stc[['h']], box.w, stc[['y1']]-stc[['h']]), 
-                            fill=colors[[from.type]], stroke='none', opacity="0.5",
-                            onmousemove=mouse.text, onmouseout="hovertext(' ');evt.target.setAttribute('opacity','0.6');"))
+                            fill=colors[[from.type]], stroke='none', opacity="0.5", id=id))
+      mouser.h <- max(min.h, stc[['h']])
+      svg_node('path', g.blank, c(d = sprintf("M%s,%s L%s,%s v-%s L%s,%s", box.w, stc[['y1']], box.w+gap.s, stc[['y2']], mouser.h, box.w, stc[['y1']]-mouser.h), 
+                            fill=colors[[from.type]], 
+                            onmousemove=sprintf("%s;changeOpacity('%s','1')",mouse.text, id), onmouseout=sprintf("hovertext(' ');changeOpacity('%s','0.8');",id)))
     }
   }
   
@@ -208,5 +222,5 @@ for (i in 1:2){
 svg_node('rect',svg, c(id="tooltip_bg", rx="2.5", ry="2.5", width="55", height="22", fill="white", 'stroke-width'="0.5", stroke="#696969", class="hidden"))
 svg_node('text',svg, c(id="tooltip", dy="-5", stroke="none", fill="#000000", 'text-anchor'="begin", class="sub-label"), XML::newXMLTextNode(' '))
 
-
+XML::addChildren(svg, kids=list(blank.arrow.g, blank.period.g))
 dinosvg:::write_svg(svg, file='sandbox/futureSuitability-desktop.svg')
