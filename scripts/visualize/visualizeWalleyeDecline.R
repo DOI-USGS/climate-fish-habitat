de0d2894ecafe079f5793c4b6bbf092256d24f4e
Lindsay Carr <lcarr@usgs.gov>
2016-06-21 16:12:41 -0500

    make is working from bash (not from build)

--- scripts/visualize/visualizeWalleyeDecline.R
+++ scripts/visualize/visualizeWalleyeDecline.R
@@ -15,22 +15,22 @@ svgFishTrends <- function(object, filename){
 #y-title {
 \tfont-size: 14.00pt; 
 }'
-  svg = svg(object, width=10, height=7, as.xml=TRUE)
+  svg <- dinosvg::svg(object, width=10, height=7, as.xml=TRUE)
   XML::newXMLNode('text', parent = svg, attrs = c(x='10', y='25', 'text-anchor'="begin", id='y-title'), XML::newXMLTextNode('Relative abundance'), at=1)
   dinosvg:::write_svg(svg, file = filename)
 }
 
 library(gsplot)
 
-visualize.visualizeFishTrends <- function(processedWallyTrends, processedBassTrends, outfile, args, ...){
-  
+visualizeData.visualizeFishTrends <- function(processedWallyTrends, processedBassTrends, ..., outfile){
+  extra <- list(...)
   fish <- ifelse(!missing(processedWallyTrends), processedWallyTrends, processedBassTrends)
 
   x.tcks = seq(1995,2010, by=5)
   y.tcks = seq(0, 1.5, by=0.25)
   par(mai=c(.5,.5,0.5,0.5))
   
-  gs.fish <- gsplot() %>% lines(fish$Year, fish$rel.abun, col=args$fig.col, ylim=c(0,1.3)) %>% 
+  gs.fish <- gsplot() %>% lines(fish$Year, fish$rel.abun, col=extra$args$fig.col, ylim=c(0,1.3)) %>% 
     axis(1, at=x.tcks, labels=x.tcks) %>% 
     axis(2, at=y.tcks, labels=y.tcks) 
   
