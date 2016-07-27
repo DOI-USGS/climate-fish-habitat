ce0133a7e5b23edb4f350bfad360503d853f2ccd
Luke Winslow <lwinslow@usgs.gov>
2016-07-27 14:08:57 -0500

    modified future warming fig and vizlab workflow integration

--- scripts/process/processFutureWarming.R
+++ scripts/process/processFutureWarming.R
@@ -1,11 +1,26 @@
 #processFutureWarming
-processData.processFutureWarming <- function(folderFutureTemps, outfile, ...){
-  # folderFutureTemps is a list of file names
-  # create a readData.dat function??? or just do read.csv??
-  futures.munged <- data.frame()
-  #TODO:load and munge data here
-  write.table(futures.munged, file = outfile, sep = '\t', row.names = FALSE)
+library(plyr)
+
+processData.processFutureWarming <- function(futureGDD, outfile, ...){
   
+  genmom = futureGDD
+  gdds = list()
+  periods = list(1989:2014, 2040:2064, 2065:2089)
+  starts = c(1989, 2040, 2065)
+  
+  for(i in 1:length(periods)){
+    gdds[[i]] = ddply(genmom[genmom$year %in% periods[[i]], ], 'lakeid', function(df){
+      
+      data.frame(med_gdd=median(df$GDD_wtr_5c))
+    })
+    
+    gdds[[i]]$period = paste0(range(periods[[i]]), collapse='-')
+    gdds[[i]]$periodi = i
+    
+  }
+  
+  all_period_gdd = do.call(rbind, gdds)
+  
+  write.table(all_period_gdd, file=outfile, sep='\t', row.names=FALSE)
 }
-  
-  
+
--- scripts/visualize/visualizeFutureWarming.R
+++ scripts/visualize/visualizeFutureWarming.R
@@ -1,6 +1,47 @@
 
+library(RColorBrewer)
+library(plyr)
 
 visualizeData.visualizeFutureWarming <- function(processedFutureWarming, outfile, ...){
-  futureTemps <- processedFutureWarming
-  #TODO: Generate visualization here.
+
+  all_period_gdd = processedFutureWarming
+  lakeids = c('2331600', '805400','146100') #Exemplar lakes: Trout Lake, Mendota, Green Lake
+  periods = list(1989:2014, 2040:2064, 2065:2089)
+  starts = sapply(periods, min)
+  cols = RColorBrewer::brewer.pal(3, 'Set1')
+  
+  # 
+  # 
+  # 
+  # genmom = read.table('d:/test/newGretchenFig/GENMOM_all_habitat.tsv', sep='\t', header=TRUE)
+  # 
+  # gdds = list()
+  # 
+  # for(i in 1:length(periods)){
+  #   gdds[[i]] = ddply(genmom[genmom$year %in% periods[[i]], ], 'lakeid', function(df){
+  # 
+  #     data.frame(med_gdd=median(df$GDD_wtr_5c))
+  #   })
+  # 
+  #   gdds[[i]]$period = paste0(range(periods[[i]]), collapse='-')
+  #   gdds[[i]]$periodi = i
+  # 
+  # }
+  # 
+  # all_period_gdd = do.call(rbind, gdds)
+  threelake = subset(all_period_gdd, lakeid %in% lakeids)
+  
+  png(filename = outfile, width=2600, height=1900, res=300)
+  
+  plot(1, NA, ylim=c(2200,3300), xlim=c(0.75,3.25), xaxt='n', xlab='', ylab='Degree Days')
+  
+  for(i in 1:length(lakeids)){
+    points(med_gdd~periodi, subset(all_period_gdd, lakeid==lakeids[i]), pch=16, col=cols[i])
+    lines(med_gdd~periodi, subset(all_period_gdd, lakeid==lakeids[i]), lty=3, col=cols[i])
+  }
+  
+  axis(1, at=1:3, labels = unlist(lapply(lapply(periods, range), paste, collapse="-")))
+  
+  dev.off()
+  
 }
